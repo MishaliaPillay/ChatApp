692ca81288966d3fbe41d977c8820b450955a46c
Mishalia Pillay <Mishalia.Pillay@boxfusion.io>
2025-02-13 16:04:46 +0200

    All the fontawesome icons are added for the nav

--- JS/nav.js
+++ JS/nav.js
@@ -1,21 +1,27 @@
 const urls=[
-    {url:'profile-page.html'},
-    {url:'contacts-page.html'},
-    {url:'general-chat-page.html'}
+    {url:'profile-page.html',iconClass:'fa fa-user'},
+    {url:'contacts-page.html',iconClass:'fa fa-address-book'},
+    {url:'general-chat-page.html',iconClass:'fa fa-comments'}
 ]
 
 
 
 const navbar=document.getElementById('navbar');
 
-const icon = document.createElement('i');
-icon.className='fa fa-user';
+
+
 
 urls.forEach(link=>{
     const anchortag=document.createElement('a');
+    const icon = document.createElement('i');
+    icon.className= link.iconClass;
     anchortag.href=link.url;
     anchortag.textContent = '';
     navbar.appendChild(anchortag);
-    anchortag.prepend(icon);
+    anchortag.appendChild(icon);
     navbar.appendChild(anchortag);
 })
+
+
+///to implement this add this to html <section class="navbar" id="navbar"></section>
+// <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
