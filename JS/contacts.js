e3515be085f13fb6bc3c6c0b5e6687b0a7b88b35
Mishalia Pillay <Mishalia.Pillay@boxfusion.io>
2025-02-17 15:32:02 +0200

    css for chat page added

--- JS/contacts.js
+++ JS/contacts.js
@@ -5,21 +5,8 @@ function loadContacts(username) {
 
   Object.entries(allUsers).forEach(([key, value]) => {
     const li = document.createElement("li");
+    const button = document.createElement("a");
     li.textContent = `${key}`;
     contactsList.appendChild(li);
   });
 }
-//clear the current active user list before updating it
-/*  console.log(allUsers);
-  let usersList = allUsers[username];
-  allUsers.username.forEach((listUsers) => {
-    const li = document.createElement("li");
-    li.textContent = JSON.stringify(listUsers);
-    contactsList.appendChild(li);
-  });
-
-  //loops through each active user and adds them to the list
-  allUsers.forEach((user) => {
-    const userItem = document.createElement("li");
-    userItem.textContent = user;
-    ContactsList.appendChild(userItem);  });*/
